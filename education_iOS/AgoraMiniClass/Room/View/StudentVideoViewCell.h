c6fe6f9151aadd1aa689745bb6fb5ec2b3f901d1
yangshangbin <1058070112@qq.com>
2019-08-13 14:09:16 +0800

    fix video nameLabel bug

--- education_iOS/AgoraMiniClass/Room/View/StudentVideoViewCell.h
+++ education_iOS/AgoraMiniClass/Room/View/StudentVideoViewCell.h
@@ -2,16 +2,18 @@
 //  StudentVideoViewCell.h
 //  AgoraMiniClass
 //
-//  Created by yangmoumou on 2019/7/17.
+//  Created by yangmoumou on 2019/8/13.
 //  Copyright © 2019 yangmoumou. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "RoomUserModel.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface StudentVideoViewCell : UICollectionViewCell
-
+@property (nonatomic, weak) UIView *videoCanvasView;
+@property (nonatomic, copy) RoomUserModel *userModel;
 @end
 
 NS_ASSUME_NONNULL_END
