b2a6fc5c252350faaf038f0e2e4cbe070d1ba128
yangshangbin <1058070112@qq.com>
2019-10-18 17:20:32 +0800

    update build file

--- education_iOS/build.sh
+++ education_iOS/build.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
+
 BUILD_DATE=`date +%Y-%m-%d-%H.%M.%S`
 ArchivePath=AgoraMiniClass-${BUILD_DATE}.xcarchive
 
-xcodebuild \
-clean -configuration Release -quiet  || exit
-echo '清理完成'
-
-echo '开始编译'
-xcodebuild \
-archive -workspace 'AgoraMiniClass.xcworkspace' \
--scheme 'AgoraMiniClass' \
--configuration Release \
--archivePath ${ArchivePath}  -quiet  || exit
-
-xcodebuild -exportArchive -archivePath ${ArchivePath} \
--configuration Release \
--exportPath .  \
--exportOptionsPlist exportPlist.plist \
--quiet || exit
-echo '编译完成'
+xcodebuild clean -workspace "AgoraMiniClass.xcworkspace" -scheme "AgoraMiniClass" -configuration Release
+xcodebuild -workspace "AgoraMiniClass.xcworkspace" -scheme "AgoraMiniClass" -archivePath ${ArchivePath} archive -quiet || exit
+xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath . -quiet || exit
