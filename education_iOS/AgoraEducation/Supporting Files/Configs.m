ddb937218e08926bfeeeff1847b87ebbd48b1dd2
SRS <sirusheng@agora.io>
2019-12-20 14:25:49 +0800

    remove redundancy code

--- education_iOS/AgoraEducation/Supporting Files/Configs.m
+++ education_iOS/AgoraEducation/Supporting Files/Configs.m
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-#define kAgoraAppid  @"aab8b8f5a8cd4469a63042fcfafe7063"
-#define kWhiteBoardToken @"WHITEcGFydG5lcl9pZD0xTnd5aDBsMW9ZazhaRWNuZG1kaWgwcmJjVWVsQnE1UkpPMVMmc2lnPWI5ZGRmNTU1YTgwM2Q0NjFjMDY5YTQ3NDA2ZDMxN2Q5NTVmYTJjYjY6YWRtaW5JZD01MjEmcm9sZT1taW5pJmV4cGlyZV90aW1lPTE2MDM3OTIwOTMmYWs9MU53eWgwbDFvWWs4WkVjbmRtZGloMHJiY1VlbEJxNVJKTzFTJmNyZWF0ZV90aW1lPTE1NzIyMzUxNDEmbm9uY2U9MTU3MjIzNTE0MDY3MTAw"
+#define kAgoraAppid  <#@"Agora AppID"#>
+#define kWhiteBoardToken <#@"netless Token"#>
 
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
