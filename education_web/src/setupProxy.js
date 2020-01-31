803585400c272c5a49aa9af6779e6e2d97f5be65
matrixbirds <yooobuntu@gmail.com>
2020-01-31 16:19:23 +0800

    fix. setup proxy

--- education_web/src/setupProxy.js
+++ education_web/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 const RTM_ENDPOINT = process.env.REACT_APP_AGORA_ENDPOINT_URL;
 const APP_ID = process.env.REACT_APP_AGORA_APP_ID;
 module.exports = function (app) {
-  app.use(
+  RTM_ENDPOINT && app.use(
     '/dev/v2/',
     proxy({
       target: RTM_ENDPOINT,
