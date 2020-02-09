5dbd674bb839970b57ac0caf103e62de81fc1329
matrixbirds <yooobuntu@gmail.com>
2020-02-09 18:57:17 +0800

    fix. color picker

--- education_web/src/components/mediaboard.tsx
+++ education_web/src/components/mediaboard.tsx
@@ -532,17 +532,16 @@ const items = [
     return null;
   }, [uploadPhase, convertPhase]);
 
-  const strokeColor: RGBColor | undefined = useMemo(() => {
-    if (whiteboardState.room) {
-      const strokeColor = whiteboardState.room.state.memberState.strokeColor;
-      return {
-        r: strokeColor[0],
-        g: strokeColor[1],
-        b: strokeColor[2],
-      }
+  let strokeColor: RGBColor | undefined = undefined;
+
+  if (whiteboardState.room && whiteboardState.room.state.memberState.strokeColor) {
+    const color = whiteboardState.room.state.memberState.strokeColor;
+    strokeColor = {
+      r: color[0],
+      g: color[1],
+      b: color[2],
     }
-    return undefined;
-  }, [whiteboardState.room]);
+  }
   
   return (
     <div className={`media-board ${drawable}`}>
@@ -570,7 +569,7 @@ const items = [
           items={toolItems}
           currentTool={tool}
           handleToolClick={handleToolClick} />
-          {tool === 'color_picker' && strokeColor ?
+          {tool === 'color_picker' && room && room.state ?
             <SketchPicker
               color={strokeColor}
               onChangeComplete={onColorChanged} />
