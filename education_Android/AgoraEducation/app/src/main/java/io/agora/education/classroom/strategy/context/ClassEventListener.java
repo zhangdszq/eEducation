20fddb2873b067263605a785950161fb9f9defc0
HUI <15215604969@163.com>
2020-02-12 12:04:51 +0800

    init

---
+++ education_Android/AgoraEducation/app/src/main/java/io/agora/education/classroom/strategy/context/ClassEventListener.java
@@ -0,0 +1,27 @@
+package io.agora.education.classroom.strategy.context;
+
+import io.agora.education.classroom.bean.msg.ChannelMsg;
+import io.agora.education.classroom.bean.user.Teacher;
+import io.agora.sdk.annotation.NetworkQuality;
+
+public interface ClassEventListener {
+
+    void onTeacherInit(Teacher teacher);
+
+    void onNetworkQualityChanged(@NetworkQuality int quality);
+
+    void onClassStateChanged(boolean isStart);
+
+    void onWhiteboardIdChanged(String id);
+
+    void onMuteLocalChat(boolean muted);
+
+    void onMuteAllChat(boolean muted);
+
+    void onChannelMsgReceived(ChannelMsg msg);
+
+    void onScreenShareJoined(int uid);
+
+    void onScreenShareOffline(int uid);
+
+}
---
+++ education_Android/AgoraEducation/app/src/main/java/io/agora/education/service/bean/response/AppConfig.java
@@ -0,0 +1,19 @@
+package io.agora.education.service.bean.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AppConfig {
+
+    public int version;
+    public String appId;
+    public String authorization;
+    @SerializedName("1on1TeacherLimit")
+    public int one2OneTeacherLimit;
+    @SerializedName("1on1StudentLimit")
+    public int one2OneStudentLimit;
+    public int smallClassTeacherLimit;
+    public int smallClassStudentLimit;
+    public int largeClassTeacherLimit;
+    public int largeClassStudentLimit;
+
+}
