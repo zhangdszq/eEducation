d8461080a11dc16e468027abce2fd5a02e06dff1
matrixbirds <yooobuntu@gmail.com>
2020-02-15 23:00:08 +0800

    feat. add rtm replay refactor. replay page

--- education_web/src/components/chat/message.tsx
+++ education_web/src/components/chat/message.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import './index.scss';
 import { Link } from 'react-router-dom';
 import { useRoomState } from '../../containers/root-container';
@@ -7,13 +7,19 @@ interface MessageProps {
   content: string
   link?: string
   sender?: boolean
+  children?: any
+  ref?: any
+  className?: string
 }
 
 export const Message: React.FC<MessageProps> = ({
   nickname,
   content,
   link,
-  sender
+  sender,
+  children,
+  ref,
+  className
 }) => {
 
   const roomState = useRoomState();
@@ -21,20 +27,21 @@ export const Message: React.FC<MessageProps> = ({
   const text = React.useMemo(() => {
     if (link && roomState.course.rid) {
       return (
-        <Link to={`${link}?rid=${roomState.course.rid}`} target="_blank">course recording</Link>
+        <Link to={`${link}?rid=${roomState.course.rid}&senderId=${roomState.me.uid}`} target="_blank">course recording</Link>
       )
     }
     return link ? link : content;
   }, [content, link, roomState.course.rid])
 
   return (
-  <div className={`message ${sender ? 'sent': 'receive'}`}>
+  <div ref={ref} className={`message ${sender ? 'sent': 'receive'} ${className ? className : ''}`}>
     <div className="nickname">
       {nickname}
     </div>
     <div className="content">
       {text}
     </div>
+    {children ? children : null}
   </div>
   )
 }
