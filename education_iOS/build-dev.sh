be343b64da5f4fbc6066aef1cc2cfad63db305e7
SRS <sirusheng@agora.io>
2020-05-07 10:31:22 +0800

    remove old ios code

--- education_iOS/build-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ArchivePath=AgoraEducationDev.xcarchive
-IPAName="IPADEV"
-
-xcodebuild clean -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation" -configuration DevRelease
-xcodebuild archive -workspace "AgoraEducation.xcworkspace" -scheme "AgoraEducation"  -configuration DevRelease -archivePath ${ArchivePath} -quiet || exit
-xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath ${IPAName} -quiet || exit
-cp ${IPAName}/AgoraEducation.ipa AgoraEducationDev.ipa
-
