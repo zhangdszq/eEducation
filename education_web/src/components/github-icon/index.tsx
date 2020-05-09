97583faa5274dc30f8189dc1b91471aab8b21c9b
matrixbirds <yooobuntu@gmail.com>
2020-05-09 11:19:23 +0800

    chore. update link

--- education_web/src/components/github-icon/index.tsx
+++ education_web/src/components/github-icon/index.tsx
@@ -3,7 +3,7 @@ import './index.scss';
 
 export const GithubIcon: React.FC<{}> = (props) => {
   return (
-    <a href="https://github.com/AgoraIO-Community/eEducation-Web" target="_blank" rel="noopener noreferrer" >
+    <a href="https://github.com/AgoraIO-Usecase/eEducation" target="_blank" rel="noopener noreferrer" >
     <span className="agora-github-pin">
     </span>
     </a>
