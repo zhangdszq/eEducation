e7e3abc149c8866d6653a0dbf0231ed903857485
matrixbirds <9990676+Matrixbirds@users.noreply.github.com>
2020-05-20 10:39:17 +0800

    Update edu-api.ts

--- education_web/src/services/edu-api.ts
+++ education_web/src/services/edu-api.ts
@@ -31,7 +31,7 @@ const AgoraFetchJson = async ({url, method, data, token, full_url}:{url?: string
     method,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Basic ${AUTHORIZATION!.replace(/basic\s+|basic/i, 'Basic ')}`
+      'Authorization': `Basic ${AUTHORIZATION!.replace(/basic\s+|basic/i, '')}`
     }
   }
 
